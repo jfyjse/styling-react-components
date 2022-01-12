e07e0f057d97e591ea5392fe84a593f9e6f4179c
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-01-12 17:59:59 +0530

    static and dynamic styling using css modules and media query

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,47 +1,45 @@
+import React, { useState } from "react";
+// import styled from 'styled-components';
 
-  
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.invalid ? 'red' : 'black')};
+//   }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'red' : 'black')};
-  }
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
-
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -52,10 +50,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
